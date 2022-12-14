b00f579b019e753270061bee83cf957a72df474b
Jasper <jasper.rouwhorst93@gmail.com>
2022-12-14 12:59:52 +0700

    Valid Parentheses

---
+++ Valid Parentheses/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Solution 
+{
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            int parenthesesCount = 0;
+            foreach (char character in input)
+            {
+                if (character == '(')
+                {
+                    parenthesesCount++;
+                }
+                else if(character == ')')
+                {
+                    parenthesesCount--;
+                    
+                    if (parenthesesCount < 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return parenthesesCount == 0;
+        }
+    }
+    
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest1()
+        {
+            Assert.AreEqual(true, Parentheses.ValidParentheses( "()" ));
+        }
+    
+        [Test]
+        public void SampleTest2()
+        {
+            Assert.AreEqual(false, Parentheses.ValidParentheses( ")((((" ));
+        }
+    }
+}
