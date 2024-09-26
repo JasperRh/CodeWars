f7e56a113dc8792a98fddf54d9427106faad2bd2
Jasper <jasper.rouwhorst93@gmail.com>
2024-09-26 11:14:30 +0200

    Your Order, Please

---
+++ YourOrderPlease/Class1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace YourOrderPlease;
+
+/// <summary>
+/// https://www.codewars.com/kata/55c45be3b2079eccff00010f
+/// </summary>
+public static class Kata
+{
+    public static string Order(string words)
+    {
+        return string.Join(' ', words.Split(' ').OrderBy(word => word.SingleOrDefault(char.IsDigit)));
+    }
+}
+
+[TestFixture]
+public class SolutionTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.AreEqual("Thi1s is2 3a T4est", Kata.Order("is2 Thi1s T4est 3a"));
+        Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", Kata.Order("4of Fo1r pe6ople g3ood th5e the2"));
+        Assert.AreEqual("", Kata.Order(""));
+    }
+}
